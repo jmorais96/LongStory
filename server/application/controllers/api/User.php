a5d368dffe9c1d67118fad036d43489f0d7db754
Unknown <jose.morais.96@hotmail.com>
2019-01-01 23:33:36 -0100

    getUser server

---
+++ server/application/controllers/api/User.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jose
+ * Date: 13-12-2018
+ * Time: 15:53
+ */
+
+use Restserver\Libraries\REST_Controller;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+//To Solve File REST_Controller not found
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+/**
+ * This is an example of a few basic user interaction methods you could use
+ * all done with a hardcoded array
+ *
+ * @package         CodeIgniter
+ * @subpackage      Rest Server
+ * @category        Controller
+ * @author          Phil Sturgeon, Chris Kacerguis
+ * @license         MIT
+ * @link            https://github.com/chriskacerguis/codeigniter-restserver
+ */
+class User extends REST_Controller {
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+
+        $this->load->model('api/user_model');
+
+    }
+
+    public function getUser_get()
+    {
+        $id= $this->get('id');
+
+        if ($id===NULL)
+            $user= $this->user_model->getUsers();
+        else
+            $user= $this->user_model->getUsers($id);
+
+        $this->response($user, REST_Controller::HTTP_OK);
+    }
+
+    function addUser_post()
+    {
+        $user = array(
+            'name' =>$this->post('name'),
+            'email' =>$this->post('email'),
+            'pass' =>$this->post('pass'),
+            'birthDate' =>$this->post('birth'),
+        );
+        $user['idProfile']=1;
+
+
+
+        if ($user['name'] == '' || $user['email']== '' || $user['pass']=="")
+        {
+            $message = [
+                'id' => -1,
+                'message' => 'não foi passivel registar o utilizador na base de dados'
+            ];
+
+            $this->set_response($message, \Restserver\Libraries\REST_Controller::HTTP_NOT_FOUND);
+            return;
+        }
+        $ret=$this->user_model->addUser($user);
+        if ($ret<0)
+        {
+            $message = [
+                'id' => -2,
+                'message' => 'não foi passivel registar o utilizador na base de dados'
+            ];
+
+            $this->set_response($message, \Restserver\Libraries\REST_Controller::HTTP_NOT_FOUND);
+            return;
+        }
+        else
+        {
+            $message = [
+                'id' => $ret,
+                'message' => 'utilizador criado',
+            ];
+
+            $this->set_response($message, \Restserver\Libraries\REST_Controller::HTTP_CREATED);
+
+        }
+
+    }
+}
---
+++ server/application/models/api/User_model.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jose
+ * Date: 18-12-2018
+ * Time: 15:37
+ */
+
+if (!defined('BASEPATH')) die();
+
+class User_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    function getUsers($id=0)
+    {
+        $this->db->select("u.idUser, u.name, u.email, u.pass, u.birthDate, p.type", false);
+        $this->db->from("user as u");
+        $this->db->join("profile as p" , "u.idProfile=p.idProfile");
+
+        if ($id != 0)
+            $this->db->where('u.idUser', $id);
+
+        $query=$this->db->get();
+
+
+        $users = array();
+        foreach ($query->result() as $t)
+            $users[] = (array) $t;
+
+        return $users;
+
+    }
+
+    function addUser($user)
+    {
+        $ret = $this->db->insert('user', $user);
+
+        if (!$ret)
+            return -1;
+
+        $user_id = $this->db->insert_id();
+        //print_r($genders_arr);
+        /*foreach ($genders_arr as $g) {
+            if ($g !=''){
+                $ret = $this->db->insert('gender_has_movie',
+                    array('movie_id' => $movie_id,
+                        'gender_id' => $g)
+                );
+                if (!$ret)
+                    return -2;
+            }
+        }*/
+
+        return $user_id;
+    }
+
+
+
+}
